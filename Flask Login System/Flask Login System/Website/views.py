52787395513d9de44ab96577b5ac98f74dbb9dcc
Shubham Dev <devshubham22@gmail.com>
2023-09-15 09:34:12 +0530

    Delete Flask Login System/Flask Login System directory
    Working on an alternate system now so this is not needed anymore

--- Flask Login System/Flask Login System/Website/views.py
+++
@@ -1,7 +0,0 @@
-from flask import Blueprint , render_template
-
-views = Blueprint('views',__name__)
-
-@views.route("/")
-def home():
-    return render_template("home.html") 
--- Flask Login System/Flask Login System/main.py
+++
@@ -1,4 +0,0 @@
-from Website import create_apps
-app = create_apps()
-if __name__=='__main__':
-    app .run(debug=True)
